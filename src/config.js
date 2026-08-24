1d59b218c443333d44d3d996c8c8aec1920e1c70
toniefilip123 <filips1212@icloud.com>
2026-08-25 00:20:03 +0200

    Add configuration for Discord bot settings

---
+++ src/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // Wpisz ID swojego serwera Discord
+  GUILD_ID: "WSTAW_ID_SERWERA",
+
+  // Kanał, na którym bot wysyła powitania
+  WELCOME_CHANNEL_ID: "WSTAW_ID_KANALU_POWITAN",
+
+  // Kanał, do którego bot wysyła logi
+  LOG_CHANNEL_ID: "WSTAW_ID_KANALU_LOGOW",
+
+  // Rola nadawana po kliknięciu weryfikacji
+  VERIFIED_ROLE_ID: "WSTAW_ID_ROLI",
+
+  // Kategoria, w której mają powstawać tickety
+  TICKET_CATEGORY_ID: "WSTAW_ID_KATEGORII_TICKETOW",
+
+  // Kolor embedów
+  EMBED_COLOR: 0x2b2d31
+};
